54e0809cf52360e61da96226a4e470c43fc73429
decten <52107658+decten@users.noreply.github.com>
2020-09-12 02:56:48 +0900

    Update 최대 힙.cpp

--- 최대 힙.cpp
+++ 최대 힙.cpp
@@ -1,51 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <queue>
 
-typedef struct node
-{
-    int data;
-    struct node* left;
-    struct node* right;
-}Node;
-
-void search(Node* node)
-{
-    if (node == NULL)
-        printf("0\n");
-    else
-    {
-        search(node->right);
-        printf("%d", node->data);
-    }
-}
-
-Node* insert(Node* node, int data)
-{
-    if (node->data > data)
-    {
-        node->left = insert(node->left, data);
-    }
-    else
-    {
-        node->right = insert(node->right, data);
-    }
-    return node;
-}
+using namespace std;
 
 int main() {
-    int n, x;
-    scanf("%d", &n);
-    Node* node = NULL;
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &x);
-        insert(node, x);
-        if (x == 0)
-        {
-            search(node);
+   priority_queue<int> pq;
+   while(n){
+        int x;
+        cin >> x;
+        pq.push(x);
+
+        if(x == 0){
+            if(pq.empty()) cout << 0 << endl;
+            else{
+                cout << pq.top()<<endl;
+                pq.pop();
+            }
         }
-    }
+        n--;
+   }
 
     return 0;
 }
