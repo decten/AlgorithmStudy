2d5346ac1600601b4fd51354aab19032e974f9f3
decten <52107658+decten@users.noreply.github.com>
2020-09-12 01:38:08 +0900

    Update 암호 제작.c

--- 암호 제작.c
+++ 암호 제작.c
@@ -3,34 +3,40 @@
 #include <math.h>
 #include <stdbool.h>
 
-bool chk(const char *s, int p) {
+bool check(const char* s, int p) 
+{
     int ret = 0;
     for (int i = 0; s[i]; i++)
         ret = (ret * 10 + s[i] - '0') % p;
-    return (ret==0) ? true : false; //ret이 0이면 나누어진 것
+    return !ret ? true : false ;
 }
-int main(){
-    bool c[1000000]= {true,};
-    int result = 0;
-    char* a;
+
+int main() {
+    bool c[1000000] = { true, };
+    char a[1000];
     int b;
-    scanf("%s %d", a,&b);
+    scanf("%s %d", a, &b);
 
-    for(int i=2; i<=sqrt(b); i++ ){
-      if (!c[i]) continue;
-      for(int j=i+i; j<=b; j+=i) 
-        c[j]=false;
+    for (int i = 2; i <= sqrt(b); i++) 
+    {
+        if (!c[i]) continue;
+        for (int j = i + i; j <= b; j += i) 
+            c[j] = true;
     }
-    
-    for(int i=2; i<b; i++){
-      if(!c[i]) continue;
-      if(chk(a,i)){
-        printf("BAD %d", i);
-        return 0;
-      }
+    bool flag = false;
+    int ret = b;
+    for (int i = 2; i < b; i++) 
+    {
+        if (c[i]) continue;
+        if( check(a,i)) 
+        {
+            flag = true;
+            ret = ((ret > i) ? i:ret);
+        }
     }
 
-    printf("GOOD");
+    printf("%s ", flag? "BAD" : "GOOD");
+    if (flag) printf("%d",ret);
 
     return 0;
 }
